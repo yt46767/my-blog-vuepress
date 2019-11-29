1288fa492eb49513aebd971c70d7476488d2453c
Administrator <admin@example.com>
2019-11-29 16:56:42 +0800

    --l

--- git.sh
+++ git.sh
@@ -68,7 +68,7 @@ for i in $*;do
             echo "git合并分支名不能为空！"
             exit
         else 
-            # 切换分支[测试2]
+            # 切换分支[测试3]
             echo "正在将分支$value1合并到分支$value..."
             git checkout "$value" && git merge "$value1"
             echo "成功将分支$value1合并到分支$value！"
